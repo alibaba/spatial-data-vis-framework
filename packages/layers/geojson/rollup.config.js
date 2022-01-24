d141c5dabbf4fbbcd3a5259c9a485c9b8d8d14bc
gaomeng1900 <gaomeng.gm@alibaba-inc.com>
2022-01-24 20:57:54 +0800

    fix web-worker usage 2

--- packages/layers/geojson/rollup.config.js
+++ packages/layers/geojson/rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import sourcemaps from 'rollup-plugin-sourcemaps'
 import webWorkerLoader from 'rollup-plugin-web-worker-loader'
-import OMT from '@surma/rollup-plugin-off-main-thread'
+// import OMT from '@surma/rollup-plugin-off-main-thread'
 
 export default {
 	output: {
