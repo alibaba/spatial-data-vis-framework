0026ba52852b4932c2f3f7b3dad2315f9a8b8ff5
yao yuan <yaoyuan.yao@alibaba-inc.com>
2022-03-16 18:15:30 +0800

    WIP: upgrade layers

--- packages/layers/helper/src/HelperLayer.ts
+++ packages/layers/helper/src/HelperLayer.ts
@@ -15,7 +15,7 @@ import { StandardLayer, StandardLayerProps } from '@polaris.gl/gsi'
  * 整个项目的 Layer 最好保持一致，使用 GL2.THREE(based on v94) 或者 THREE@latest.
  * 如果希望 Layer 能够跨渲染器通用，则应该使用 Renderable(GSI) 接口。
  */
-import { Mesh, Geom, MatrUnlit, Attr } from '@gs.i/frontend-sdk'
+import { Mesh, Geom, UnlitMaterial, Attr } from '@gs.i/frontend-sdk'
 import { buildBox } from '@gs.i/utils-geom-builders'
 
 /**
@@ -37,7 +37,6 @@ export type HelperLayerProps = StandardLayerProps & typeof defaultProps
 export class HelperLayer extends StandardLayer<HelperLayerProps> {
 	axises: Mesh
 	box?: Mesh
-	props: any
 
 	constructor(props: Partial<HelperLayerProps> = {}) {
 		const _props = {
@@ -46,91 +45,98 @@ export class HelperLayer extends StandardLayer<HelperLayerProps> {
 		}
 		super(_props)
 
-		this.props = _props
-
-		// 在 场景中加入 坐标轴
-		this.listenProps(['length', 'box'], () => {
-			if (this.axises) {
-				this.group.remove(this.axises)
-			}
-
-			const length = this.getProps('length')
-			const lineGeom = new Geom({
-				mode: 'LINES',
-				attributes: {
-					position: new Attr(
-						new Float32Array([
-							0.0,
-							0.0,
-							0.0,
-							length,
-							0.0,
-							0.0,
-							0.0,
-							0.0,
-							0.0,
-							0.0,
-							length,
-							0.0,
-							0.0,
-							0.0,
-							0.0,
-							0.0,
-							0.0,
-							length,
-						]),
-						3
-					),
-					vertexColor: new Attr(
-						new Float32Array([
-							1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
-							1.0,
-						]),
-						3
-					),
-				},
-			})
-
-			const lineMatr = new MatrUnlit({})
-
-			lineMatr.vertGlobal = `
-				attribute vec3 vertexColor;
-				varying vec3 vColor;
-			`
-			lineMatr.fragGlobal = `
-				varying vec3 vColor;
-			`
-			lineMatr.vertGeometry = `vColor = vertexColor;`
-			lineMatr.fragOutput = `fragColor = vec4(vColor, 1.0);`
-
-			this.axises = new Mesh({
-				geometry: lineGeom,
-				material: lineMatr,
-			})
-
-			this.group.add(this.axises)
-
-			if (this.box) {
-				this.group.remove(this.box)
-				this.box = undefined
-			}
-
-			if (this.getProps('box')) {
-				const geom = buildBox({
-					width: this.getProps('length') * 0.1,
-					height: this.getProps('length') * 0.1,
-					depth: this.getProps('length') * 0.1,
-				})
-
-				this.box = new Mesh({
-					geometry: geom,
-					material: new MatrUnlit({ baseColorFactor: { r: 1, g: 0, b: 1 } }),
-				})
-
-				this.group.add(this.box)
-			}
+		this.addEventListener('init', (e) => {
+			this._init()
 		})
 	}
 
-	init() {}
+	_init() {
+		// 在 场景中加入 坐标轴
+		this.watchProps(
+			['length', 'box'],
+			(e) => {
+				const length = this.getProp('length')
+				const lineGeom = new Geom({
+					mode: 'LINES',
+					attributes: {
+						position: new Attr(
+							new Float32Array([
+								0.0,
+								0.0,
+								0.0,
+								length,
+								0.0,
+								0.0,
+								0.0,
+								0.0,
+								0.0,
+								0.0,
+								length,
+								0.0,
+								0.0,
+								0.0,
+								0.0,
+								0.0,
+								0.0,
+								length,
+							]),
+							3
+						),
+						vertexColor: new Attr(
+							new Float32Array([
+								1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+								1.0,
+							]),
+							3
+						),
+					},
+				})
+
+				const lineMatr = new UnlitMaterial({})
+
+				lineMatr.vertGlobal = `
+				attribute vec3 vertexColor;
+				varying vec3 vColor;
+			`
+				lineMatr.fragGlobal = `
+				varying vec3 vColor;
+			`
+				lineMatr.vertGeometry = `vColor = vertexColor;`
+				lineMatr.fragOutput = `fragColor = vec4(vColor, 1.0);`
+
+				if (this.axises) {
+					this.group.remove(this.axises)
+				}
+
+				this.axises = new Mesh({
+					geometry: lineGeom,
+					material: lineMatr,
+				})
+
+				this.group.add(this.axises)
+
+				// remove box
+				if (this.box) {
+					this.group.remove(this.box)
+					this.box = undefined
+				}
+
+				if (this.getProp('box')) {
+					const geom = buildBox({
+						width: length * 0.1,
+						height: length * 0.1,
+						depth: length * 0.1,
+					})
+
+					this.box = new Mesh({
+						geometry: geom,
+						material: new UnlitMaterial({ baseColorFactor: { r: 1, g: 0, b: 1 } }),
+					})
+
+					this.group.add(this.box)
+				}
+			},
+			{ immediate: true }
+		)
+	}
 }
