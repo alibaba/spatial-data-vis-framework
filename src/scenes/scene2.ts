d41ca340384970a5e5d7a782d824fb1764946ed8
gaomeng1900 <gaomeng1900@gmail.com>
2022-08-31 23:10:23 +0800

    temp

--- src/scenes/scene2.ts
+++ src/scenes/scene2.ts
@@ -8,7 +8,7 @@ const scene = new SceneBase()
 scene.id = 'LOCAL_SCENE_2'
 scene.name = 'scene2'
 scene.cameraStateCode = '1|0.000200|0.000943|0.000000|0.99540|-0.48000|19.27600'
-scene.layers = ['LOCAL_LAYER_0', 'LOCAL_LAYER_1', 'LOCAL_LAYER_3']
+scene.layers = ['LOCAL_LAYER_1', 'LOCAL_LAYER_3']
 
 occupyID(scene.id)
 
