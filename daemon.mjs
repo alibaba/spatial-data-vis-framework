65686e12aeedda4f64dcc40b782f2620d7293e6a
gaomeng1900 <gaomeng1900@gmail.com>
2023-03-18 15:48:18 +0800

    fix: daemon limit

--- daemon.mjs
+++ daemon.mjs
@@ -47,9 +47,9 @@ let job = null
 
 const app = express()
 app.use(cors())
-app.use(express.json())
-app.use(express.text())
-app.use(express.raw())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.text({ limit: '50mb' }))
+app.use(express.raw({ limit: '50mb' }))
 app.use(function (error, req, res, next) {
 	if (error instanceof SyntaxError) {
 		// body parser error
